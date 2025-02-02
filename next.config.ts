1c870099601cde3ec0714eb386ffdf55ece36a89
Avner Adda <avner.adda@outlook.com>
2025-02-02 18:06:58 +0200

    okk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.GITHUB_ACTIONS ? '/avneradda.github.io' : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/avneradda.github.io/' : '',
 };
 
 export default nextConfig;
